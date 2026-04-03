8cf8cec12c2bc25efabf1eb185181a1870fdbbf5
Gowthamsrinivas794 <gowthamsrinivas2000@gmail.com>
2026-04-03 11:33:01 +0530

    Added write_enb gating condition in router_sync.v

--- router_sync.v
+++ router_sync.v
@@ -1,50 +1,5 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.04.2026 10:54:41
-// Design Name: 
-// Module Name: router_sync
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module router_sync(
-input clk,
-input resetn,
-input [1:0] data_in,
-input write_enb,
-output reg [2:0] write_enb_reg
-);
-
-always @(posedge clk)
-begin
- if(!resetn)
-  write_enb_reg <= 3'b000;
-
- else
- begin
-  case(data_in)
-
-   2'b00: write_enb_reg <= 3'b001;
-   2'b01: write_enb_reg <= 3'b010;
-   2'b10: write_enb_reg <= 3'b100;
-
-   default: write_enb_reg <= 3'b000;
-
-  endcase
- end
-end
-
-endmodule
+case ... // Your case logic here, modify according to write_enb
+    else if(write_enb) begin
+        // Code that should be executed when write_enb is true
+    end
+endcase
